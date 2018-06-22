17a6ac03deb1e61a7524169a0c8527e704fa757f
angel539 <angelmoraseg@gmail.com>
2018-06-22 13:17:34 +0200

    everything deleted to clean trash files

--- uam.extremo.ui.views.extensions/src/uam/extremo/ui/views/extensions/actions/IExtensibleViewPartActionContribution.java
+++
@@ -1,8 +0,0 @@
-package uam.extremo.ui.views.extensions.actions;
-
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IEditorPart;
-
-public interface IExtensibleViewPartActionContribution {
-	public void execute(IEditorPart editorInput, ISelection selection, String editorID);
-}
